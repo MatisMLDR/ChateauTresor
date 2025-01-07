00f29124871d18fc1387c37c3df112f933b82450
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-01-07 10:03:25 +0100

    Drop.sql afin de supprimer toutes les tables, tous les triggers et toutes les fonctions de la base de données

---
+++ utils/db/drop.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS
+    public.Chateau,
+    public.Equipe_Organisatrice,
+    public.Membre_equipe,
+    public.Appartenance_Equipe,
+    public.Participant,
+    public.Chasse,
+    public.Participation,
+    public.Avis,
+    public.Recompense,
+    public.Enigme,
+    public.Indice,
+    public.Image_indice,
+    public.Son_indice,
+    public.Texte,
+    public.Haut_fait,
+    public.Profiles,
+    public.proprietaire_chateau
+CASCADE;
+
+DROP TRIGGER IF EXISTS
+    on_auth_user_created ON auth.users;
+
+DROP FUNCTION IF EXISTS
+    public.handle_new_user;
