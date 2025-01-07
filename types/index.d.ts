5c845f15706422c052b24a83068fabce6c5945eb
Simon <simzeru@gmail.com>
2025-01-07 14:27:58 +0100

    Modification des types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,3 @@
-declare type SearchParamProps = {
-  params: { [key: string]: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
 interface StripeProduct {
   id: string;
   name: string;
@@ -23,4 +18,83 @@ interface SliderWithBoundsProps {
   min: number;
   max: number;
   step: number;
+  className?: string;
 }
+
+interface TitleTwoProps {
+  text: string;
+  color: 'light' | 'dark';
+  className?: string;
+}
+
+interface NavigationVerticaleItemsProps {
+  name: string;
+  link: string;
+  imagePath: string;
+  className?: string;
+}
+
+
+
+//// Création de chasse au trésor ////
+
+export type Castle = {
+  id: string;
+  name: string;
+  description: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  imageUrl: string;
+  address: string;
+};
+
+export type ClueType = 'image' | 'sound' | 'text';
+
+export type Clue = {
+  id: string;
+  type: ClueType;
+  content: string;
+};
+
+export type Riddle = {
+  id: string;
+  question: string;
+  clues: Clue[];
+  qrCode: string;
+  code: string;
+};
+
+export type TreasureHunt = {
+  id: string;
+  title: string;
+  description: string;
+  castle: Castle;
+  price: number;
+  duration: number;
+  difficulty: 'easy' | 'medium' | 'hard';
+  riddles: Riddle[];
+  imageUrl: string;
+  createdBy: string;
+};
+
+type Chasse = {
+  id_chasse: number;
+  titre: string;
+  description: string;
+  image: string;
+  difficulte: number;
+  prix: number;
+  date_debut: string;
+  date_fin: string;
+};
+
+type Chateau = {
+  id_chateau: number;
+  nom: string;
+  description: string;
+  image: string;
+  localisation: string; // Format : "latitude,longitude"
+  chasses?: Chasse[];
+};
