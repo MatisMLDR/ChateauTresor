625f4909e652dd6e94d21a0a2a7e786a1645d617
Simon <simzeru@gmail.com>
2025-01-07 16:55:31 +0100

    Update background color in NavigationVerticale component

--- components/ui/navigationverticale.tsx
+++ components/ui/navigationverticale.tsx
@@ -11,7 +11,7 @@ export function NavigationVerticale() {
 
 
   return (
-    <div className="relative flex h-screen w-[10vw] transition-all flex-col items-center justify-end bg-[#12304A]">
+    <div className="relative flex h-screen w-[10vw] transition-all flex-col items-center justify-end bg-primary">
       <NavigationVerticale_Bouton name={'book'} link={'/participants/historique'} imagePath={'/book.svg'} />
       <NavigationVerticale_Bouton name={'map'} link={'/participants'} imagePath={'/map.svg'} />
       <NavigationVerticale_Bouton name={'castle'} link={'/participants/chateaux'} imagePath={'/castle.svg'} />
