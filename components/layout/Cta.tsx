06e46b21062c1484a2125988760eb278a0fc0c62
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-01-07 10:32:20 +0100

    Utilisation du composant H2 sur la landing page

--- components/layout/Cta.tsx
+++ components/layout/Cta.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import { TitleTwo } from '../ui/TitleTwo'
 
 const Cta = () => {
   return (
     <section className="w-full py-10 md:py-20 lg:py-32 bg-[#1a3b5d]" id="cta">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center text-white">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-            Prêt à révolutionner votre organisation ?
-          </h2>
+          <TitleTwo text={"Prêt à révolutionner votre organisation ?"} color={"light"} />
           <p className="max-w-[600px] text-lg md:text-xl">
             Inscrivez-vous maintenant et accédez gratuitement à toutes les fonctionnalités premium.
           </p>
--- components/layout/Testimonials.tsx
+++ components/layout/Testimonials.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardContent } from '../ui/card'
 import { Star } from 'lucide-react'
+import { TitleTwo } from "@/components/ui/TitleTwo";
 
 const Testimonials = () => {
   const testimonials = [
@@ -27,9 +28,7 @@ const Testimonials = () => {
   return (
     <section className="w-full py-10 md:py-20 lg:py-32 bg-[#f7f7f7]" id="testimonials">
       <div className="container px-4 md:px-6">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12 text-[#2b4e73]">
-          Ce que disent nos utilisateurs
-        </h2>
+        <TitleTwo text={"Ce que disent nos utilisateurs"} color={"dark"} />
         <div className="grid gap-10 md:grid-cols-3">
           {testimonials.map((testimonial, index) => (
             <div
@@ -42,7 +41,7 @@ const Testimonials = () => {
                 className="w-16 h-16 rounded-full object-cover"
               />
               <blockquote className="text-center text-[#6c757d]">
-                <p className="italic">"{testimonial.text}"</p>
+                <p className="italic">{testimonial.text}</p>
               </blockquote>
               <div className="text-center">
                 <p className="font-bold text-[#2b4e73]">{testimonial.name}</p>
