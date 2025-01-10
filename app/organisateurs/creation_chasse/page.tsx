e211290e736af26014cf0583169fe25bc17283ae
matismld <matis.malandrino@etu.univ-grenoble-alpes.fr>
2025-01-10 13:11:40 +0100

    Ajout des dernières pages manquantes pour les organisateurs et participants et ajout de la sidebar dans toutes les pages.

--- app/organisateurs/creation_chasse/page.tsx
+++ app/organisateurs/creation_chasse/page.tsx
@@ -2,15 +2,19 @@
 
 import { CreateHuntForm } from "@/components/organisateurs/create/create-hunt-form";
 import { contenuTextuel } from "@/lib/contenuCreationChasse";
+import { SideBarHuntCreator } from '@/components/ui/SideBarHuntCreator';
 
 export default function CreateHuntPage() {
   
   return (
-    <main className="container mx-auto px-4 py-8">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-8">{contenuTextuel.create.title}</h1>
-        <CreateHuntForm />
-      </div>
-    </main>
-  );
-}
+    <div className="flex">
+      <SideBarHuntCreator />
+      <main className="container mx-auto px-4 py-8">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-4xl font-bold mb-8">{contenuTextuel.create.title}</h1>
+          <CreateHuntForm />
+        </div>
+      </main>
+    </div>
+      );
+      }
