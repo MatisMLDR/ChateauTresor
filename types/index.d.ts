dda727f548765ebf9a2ea88216fa53b9318d946e
matismld <matis.malandrino@etu.univ-grenoble-alpes.fr>
2025-01-13 16:35:13 +0100

    Modification des types. Les id sont maintenant des UUID.

--- types/index.d.ts
+++ types/index.d.ts
@@ -53,20 +53,20 @@ export type TypeIndice = 'image' | 'sound' | 'text';
 
 // Indice
 export type IndiceType = {
-  id_indice: number; // Identifiant unique de l'indice
+  id_indice: UUID; // Identifiant unique de l'indice
   contenu: string;   // Contenu de l'indice (par défaut : 'Pas de contenu')
   ordre: number;     // Ordre de l'indice (par défaut : 1)
   degre_aide: number; // Degré d'aide, doit être compris entre 1 et 5
   type: TypeIndice; // Type de l'indice
-  id_enigme: number; // Identifiant de l'énigme associée
+  id_enigme: UUID; // Identifiant de l'énigme associée
 };
 
 // Énigme
 export type EnigmeType = {
-  id_enigme: number;
-  id_chasse: number;
+  id_enigme: UUID;
+  id_chasse: UUID;
   titre: string;
-  indices: Indice[];
+  indices?: Indice[];
   code_reponse: string;
   ordre: number;
   description: string;
@@ -81,7 +81,7 @@ export type EnigmeType = {
 ///////////
 
 export type ChasseType = {
-  id_chasse: number; // Identifiant unique, SERIAL, non null
+  id_chasse: UUID; // Identifiant unique, SERIAL, non null
   titre?: string; // Optionnel, par défaut 'Nouvelle Chasse'
   capacite?: number; // Optionnel, par défaut 0
   description?: string; // Optionnel, par défaut 'Pas de description'
@@ -96,18 +96,19 @@ export type ChasseType = {
   duree_estime?: string; // Optionnel, INTERVAL, par défaut '00:00:00'
   theme?: string; // Optionnel, par défaut 'Aucun thème'
   statut?: string; // Optionnel, par défaut 'Inactif'
-  id_chateau?: number | null; // Peut être null, clé étrangère vers Chateau(id_chateau)
-  id_equipe?: number | null; // Peut être null, clé étrangère vers Equipe_Organisatrice(id_equipe)
+  id_chateau?: UUID | null; // Peut être null, clé étrangère vers Chateau(id_chateau)
+  id_equipe?: UUID | null; // Peut être null, clé étrangère vers Equipe_Organisatrice(id_equipe)
   chateau?: ChateauType;
   enigmes?: EnigmeType[];
+  rewards?: RecompenseType[];
 };
 
 export type ParticipantType = {
-  id_participant: number;
+  id_participant: UUID;
   nom: string;
   prenom: string;
   email: string;
-  id_user: string;
+  id_user: UUID;
   adresse: string;
   ville: string;
   code_postal: string;
@@ -119,7 +120,7 @@ export type ParticipantType = {
 }
 
 export type ChateauType = {
-  id_chateau: number; // Clé primaire, identifiant du château
+  id_chateau: UUID; // Clé primaire, identifiant du château
   nom?: string; // Nom du château, par défaut 'Château inconnu'
   adresse_postale?: string; // Adresse postale, par défaut 'Non spécifiée'
   localisation?: string; // Localisation, par défaut 'Non spécifiée'
@@ -129,7 +130,7 @@ export type ChateauType = {
   description?: string; // Description, par défaut 'Pas de description'
   image?: string | null; // URL de l'image, par défaut null
   site_web?: string | null; // URL du site web, par défaut null
-  id_proprietaire?: number | null; // Clé étrangère vers Proprietaire_Chateau, par défaut null
+  id_proprietaire?: UUID | null; // Clé étrangère vers Proprietaire_Chateau, par défaut null
 
   chasses?: ChasseType[]; // Liste des chasses liées au château
 };
@@ -158,18 +159,18 @@ export type LineChart = {
 }
 
 export type AvisType = {
-  id_avis: number;
+  id_avis: UUID;
   note: number;
   titre: string;
   description: string;
   nb_like: number;
   date_modification: string;
-  id_chasse: number;
-  id_participant: number;
+  id_chasse: UUID;
+  id_participant: UUID;
 }
 
 export type ProfilType = {
-  id: string; // UUID, non null
+  id: UUID; // UUID, non null
   username?: string; // Optionnel, défaut 'anonyme'
   updated_at?: string | null; // Timestamp ISO 8601, nullable
   email: string; // Unique et non null
@@ -184,23 +185,17 @@ export type ProfilType = {
   plan: string; // Non null, défaut 'none'
 }
 
-export type TexteType = {
-  id_texte: number;
-  contenu: string;
-  id_indice: number | null;
-}
-
 /* A modifier avec les ? */
 export type MembreEquipeType = {
   id_membre: number; // Identifiant unique, SERIAL, non null
   carte_identite?: string | null; // Peut être null, sinon VARCHAR(255)
   est_verifie?: boolean; // Optionnel, par défaut false
   role_equipe?: string; // Optionnel, par défaut 'Membre'
-  id_user: string; // UUID, non null
+  id_user: UUID; // UUID, non null
 }
 
 export type Haut_FaitType = {
-  id_haut_fait: number;
+  id_haut_fait: UUID;
   titre: string;
   description: string;
   condition: string;
@@ -209,7 +204,7 @@ export type Haut_FaitType = {
 }
 
 export type EquipeOrganisatriceType = {
-  id_equipe: number;              // SERIAL PRIMARY KEY, donc un nombre entier auto-incrémenté
+  id_equipe: UUID;              // SERIAL PRIMARY KEY, donc un nombre entier auto-incrémenté
   type: string;                   // VARCHAR(255), avec une valeur par défaut 'Association'
   n_siret: string | null;         // VARCHAR(255), peut être null
   id_taxes: string | null;        // VARCHAR(255), peut être null
@@ -217,11 +212,11 @@ export type EquipeOrganisatriceType = {
   site_web: string | null;        // VARCHAR(255), peut être null
   adresse_postale: string;        // VARCHAR(255), avec une valeur par défaut 'Non spécifiée'
   telephone: string | null;       // VARCHAR(20), peut être null
-  id_user: string;                // UUID, obligatoire, avec une contrainte de clé étrangère
+  id_user: UUID;                // UUID, obligatoire, avec une contrainte de clé étrangère
 }
 
 export type RecompenseType = {
-  id_recompense?: number;
+  id_recompense?: UUID;
   nom: string;
   description: string;
   type: string;
@@ -230,7 +225,7 @@ export type RecompenseType = {
   prix_reel: number;
   image: string | null;
   date_modification: string;
-  id_chasse: number | null;
+  id_chasse: UUID | null;
 }
 
 //// Props de la sidebar ////
