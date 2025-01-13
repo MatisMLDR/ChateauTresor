564b3886d5e6e65cfa1e179bb305f42e4592a257
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-01-13 09:19:17 +0100

    2nd Sidebar pour les organisateurs

--- components/ui/SideBarCreator.tsx
+++ components/ui/SideBarCreator.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebarCreator } from "@/components/app-sidebar-creator"
 import {
     SidebarInset,
     SidebarProvider,
@@ -9,7 +9,7 @@ import React from "react";
 export function SideBarCreator({ children, }: Readonly<{ children: React.ReactNode; }> ) {
     return (
         <SidebarProvider>
-            <AppSidebar />
+            <AppSidebarCreator />
             <SidebarInset>
                 <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
