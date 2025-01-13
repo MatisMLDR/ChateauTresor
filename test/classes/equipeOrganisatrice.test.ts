8ee7ed45db8696ca76ae644224e44df066ec256c
Paul <paul.moncenix-larue@etu.univ-grenoble-alpes.fr>
2025-01-13 13:55:56 +0100

    Création de tests pour la classe EquipeOrganisatrice

---
+++ test/classes/equipeOrganisatrice.test.ts
@@ -0,0 +1,64 @@
+import EquipeOrganisatrice from "@/classes/EquipeOrganisatrice";
+import Chasse from "@/classes/EquipeOrganisatrice";
+import {
+  getEquipeById
+} from "@/utils/dao/EquipeOrganisatriceUtils";
+
+describe("Tests create()", () => {
+  it("devrait créer une chasse sans renvoyer une erreur", async () => {
+    const equipe = new EquipeOrganisatrice({ 
+        id_equipe: 999,
+        type: 'Entreprise',
+        n_siret:  '123456789',
+        id_taxes:   '987654321',
+        nb_membres: 4,
+        site_web:  'https://www.jaipasdesiteinternet.com',
+        adresse_postale: 'J habite là',
+        telephone:  '0405060708',
+        id_user: '514e8181-1f82-4f81-8c16-00bf216d3b0a',
+    });
+
+    await expect(equipe.create()).resolves.not.toThrow(Error);
+    await expect(EquipeOrganisatrice.readId(999)).resolves.toBeDefined();
+  });
+});
+
+describe("Test API", () => {
+  it("devrait récupérer l'équipe organisatrice d'ID 1 depuis la base de données", async () => {
+    const equipe = await getEquipeById(1);
+    expect(equipe).toBeDefined();
+    expect(equipe.id_equipe).toEqual(1);
+  });
+});
+
+describe("Tests readId()", () => {
+  it("devrait récupérer l'équipe organisatrice d'ID 1 depuis la base de données", async () => {
+    const equipe = await EquipeOrganisatrice.readId(1);
+    expect(equipe).toBeDefined();
+    expect(equipe.id_equipe).toEqual(1);
+  });
+  it("devrait throw une erreur car l'id est inexistant", async () => {
+    await expect(EquipeOrganisatrice.readId(9999999999)).rejects.toThrow(Error);
+  });
+});
+
+describe("Tests delete()", () => {
+  it("", async () => {
+    const equipe = await EquipeOrganisatrice.readId(999);
+    await equipe.delete();
+
+    expect(equipe.read()).toThrow('Equipe does not exist');
+  });
+});
+
+describe("Tests update()", () => {
+  it("", async () => {
+    const equipe = await EquipeOrganisatrice.readId(1);
+    const randomInt = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    equipe.setAdressePostale(`${randomInt} place de La Place`);
+    await equipe.update();
+    const equipe1 = await Chasse.readId(1);
+    expect(equipe.getAdressePostale()).toEqual(equipe1.getAdressePostale());
+  });
+});
+
