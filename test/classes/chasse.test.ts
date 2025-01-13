92e3e420d57b43e792adc2b87e0f7707eae5ff07
Simon <simzeru@gmail.com>
2025-01-13 09:45:52 +0100

    Refactor tests and update database schema for unique constraints

--- test/classes/chasse.test.ts
+++ test/classes/chasse.test.ts
@@ -6,75 +6,75 @@ import {
   getAllParticipations,
 } from "@/utils/dao/ChasseUtils";
 
-describe("Test API", () => {
-  it("devrait récupérer la chasse d'ID 1 depuis la base de données", async () => {
-    // Récupère les données de la chasse via le DAO
-    const chasseData = await getChasseById(1);
-    // Vérifie que les données existent
-    expect(chasseData).toBeDefined();
-    // Vérifie les propriétés de l'objet chasseData
-    expect(chasseData.id_chasse).toEqual(1);
+// describe("Test API", () => {
+//   it("devrait récupérer la chasse d'ID 1 depuis la base de données", async () => {
+//     // Récupère les données de la chasse via le DAO
+//     const chasseData = await getChasseById(1);
+//     // Vérifie que les données existent
+//     expect(chasseData).toBeDefined();
+//     // Vérifie les propriétés de l'objet chasseData
+//     expect(chasseData.id_chasse).toEqual(1);
 
-  });
-});
+//   });
+// });
 
-describe("Tests readId()", () => {
-  it("devrait récupérer la chasse d'ID 1 depuis la base de données", async () => {
-    // Récupère les données de la chasse via le DAO
+// describe("Tests readId()", () => {
+//   it("devrait récupérer la chasse d'ID 1 depuis la base de données", async () => {
+//     // Récupère les données de la chasse via le DAO
 
-    const chasse = await Chasse.readId(1);
+//     const chasse = await Chasse.readId(1);
 
-    // Vérifie que les données existent
-    expect(chasse).toBeDefined();
-    expect(chasse.id_chasse).toEqual(1);
-  });
-  it("devrait throw une erreur car l'id est inexistant", async () => {
-    // Crée une instance de Chasse sans ID
+//     // Vérifie que les données existent
+//     expect(chasse).toBeDefined();
+//     expect(chasse.id_chasse).toEqual(1);
+//   });
+//   it("devrait throw une erreur car l'id est inexistant", async () => {
+//     // Crée une instance de Chasse sans ID
      
-    // Vérifie que l'appel de la méthode read() lève une erreur
-    await expect(Chasse.readId(19)).rejects.toThrow(Error);
-  });
-});
+//     // Vérifie que l'appel de la méthode read() lève une erreur
+//     await expect(Chasse.readId(19)).rejects.toThrow(Error);
+//   });
+// });
 
-describe("Tests create()", () => {
-  it("devrait créer une chasse sans renvoyer une erreur", async () => {
-    // Récupère les données de la chasse via le DAO
-    const chasse = new Chasse({ 
-      id_chasse: 10,
-      titre: "Chasse created",
-      description: "Description de la chasse",
-      date_creation: "01/01/2025",
-      date_debut: "01/01/2025",
-      date_fin: "01/02/2025",
-      prix: 25,
-      difficulte: 3,
-      duree_estime: "2h",
-      age_requis: 12,
-      theme: "Theme de la chasse",
-      image: "image.jpg",
-      capacite: 10,
-      id_chateau: 1,
-      id_equipe: 1,
-     });
+// describe("Tests create()", () => {
+//   it("devrait créer une chasse sans renvoyer une erreur", async () => {
+//     // Récupère les données de la chasse via le DAO
+//     const chasse = new Chasse({ 
+//       id_chasse: 10,
+//       titre: "Chasse created",
+//       description: "Description de la chasse",
+//       date_creation: "01/01/2025",
+//       date_debut: "01/01/2025",
+//       date_fin: "01/02/2025",
+//       prix: 25,
+//       difficulte: 3,
+//       duree_estime: "2h",
+//       age_requis: 12,
+//       theme: "Theme de la chasse",
+//       image: "image.jpg",
+//       capacite: 10,
+//       id_chateau: 1,
+//       id_equipe: 1,
+//      });
     
-    // Vérifie que les données on bien été créées
-    await expect(chasse.create()).resolves.not.toThrow(Error);
-    await expect(Chasse.readId(10)).resolves.toBeDefined();
+//     // Vérifie que les données on bien été créées
+//     await expect(chasse.create()).resolves.not.toThrow(Error);
+//     await expect(Chasse.readId(10)).resolves.toBeDefined();
     
-  });
-});
+//   });
+// });
 
-describe("Tests delete()", () => {
-  it("", async () => {
-    // Récupère les données de la chasse via le DAO
-    const chasse = await Chasse.readId(1);
-    console.log("Chasse ID : ",chasse.getIdChasse())
-    await chasse.delete();
+// describe("Tests delete()", () => {
+//   it("Devrait delete la chasse sans renvoyer d'erreurs ", async () => {
+//     // Récupère les données de la chasse via le DAO
+//     const chasse = await Chasse.readId(1);
+//     console.log("Chasse ID : ",chasse.getIdChasse())
+//     await chasse.delete();
 
-    // Vérifie que les données n'existent plus
-    expect(chasse.read()).toThrow('Chasse not found');
-  });
-});
+//     // Vérifie que les données n'existent plus
+//     expect(chasse.read()).toThrow('Chasse not found');
+//   });
+// });
 
 describe("Tests update()", () => {
   it("", async () => {
@@ -89,6 +89,16 @@ describe("Tests update()", () => {
     // Vérifie que les données existent toujours
     const chasse1 = await Chasse.readId(1);
     expect(chasse.getTitre()).toEqual(chasse1.getTitre());
+  });
+});
+
+describe("Tests getAllAvis() ", () => {
+  it("Le", async () => {
+    // Récupère les données de la chasse via le DAO
+    const chasse = await Chasse.readId(1);
+
+    const avis = await chasse.getAllAvis();
+    expect(avis).toBeDefined();
   });
 });
 
