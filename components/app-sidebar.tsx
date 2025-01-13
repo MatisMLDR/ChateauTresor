65e42a1050d31b15832a68d8c802c65da0565d1b
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2025-01-13 16:37:45 +0100

    Remplissage de la navbar basé sur la base de donnée & Code Formating

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,211 +1,205 @@
-"use client"
+'use client';
 
-import * as React from "react"
+import * as React from 'react';
 
-import { NavMain } from "./nav-main"
-import { NavProjects } from "./nav-projects"
-import { NavUser } from "./nav-user"
-import { TeamSwitcher } from "./team-switcher"
+import { NavMain } from './nav-main';
+import { NavProjects } from './nav-projects';
+import { NavUser } from './nav-user';
+import { TeamSwitcher } from './team-switcher';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from '@/components/ui/sidebar';
 
 import {
-    AudioWaveform,
-    Command,
-    GalleryVerticalEnd,
-    Settings2,
-    LayoutDashboard,
-    MapPin,
-    Castle,
-    Search,
-    History,
-} from "lucide-react"
-
-const dataUser = {
-    teams: [
-        {
-            name: "Prestige Heritage",
-            logo: GalleryVerticalEnd,
-            plan: "School",
-        },
-        {
-            name: "Dream Maker",
-            logo: AudioWaveform,
-            plan: "Organisator",
-        },
-        {
-            name: "Historical Life",
-            logo: Command,
-            plan: "Client",
-        },
-    ],
-    navMain: [
-        {
-            title: "Carte",
-            url: "#",
-            icon: MapPin,
-            isActive: true,
-        },
-        {
-            title: "Châteaux",
-            url: "#",
-            icon: Castle,
-        },
-        {
-            title: "Chasses",
-            url: "#",
-            icon: Search,
-        },
-        {
-            title: "Historique",
-            url: "#",
-            icon: History,
-        },
-        {
-            title: "Paramètres",
-            url: "#",
-            icon: Settings2,
-            items: [
-                {
-                    title: "General",
-                    url: "#",
-                },
-                {
-                    title: "Team",
-                    url: "#",
-                },
-                {
-                    title: "Billing",
-                    url: "#",
-                },
-                {
-                    title: "Limits",
-                    url: "#",
-                },
-            ],
-        },
-    ],
-    chasses: [
-        {
-            name: "Design Engineering",
-            url: "#",
-            id: 1,
-        },
-        {
-            name: "Sales & Marketing",
-            url: "#",
-            id: 2,
-        },
-        {
-            name: "Travel",
-            url: "#",
-            id: 3,
-        },
-    ],
-}
-
-const dataOrganisateur = {
-    teams: [
-        {
-            name: "Prestige Heritage",
-            logo: GalleryVerticalEnd,
-            plan: "School",
-        },
-        {
-            name: "Dream Maker",
-            logo: AudioWaveform,
-            plan: "Organisator",
-        },
-        {
-            name: "Historical Life",
-            logo: Command,
-            plan: "Client",
-        },
-    ],
-    navMain: [
-        {
-            title: "Dashboard",
-            url: "#",
-            icon: LayoutDashboard,
-        },
-        {
-            title: "Châteaux",
-            url: "#",
-            icon: Castle,
-        },
-        {
-            title: "Chasses",
-            url: "#",
-            icon: Search,
-        },
-        {
-            title: "Settings",
-            url: "#",
-            icon: Settings2,
-            items: [
-                {
-                    title: "General",
-                    url: "#",
-                },
-                {
-                    title: "Team",
-                    url: "#",
-                },
-                {
-                    title: "Billing",
-                    url: "#",
-                },
-                {
-                    title: "Limits",
-                    url: "#",
-                },
-            ],
-        },
-    ],
-    chasses: [
-        {
-            name: "Design Engineering",
-            url: "#",
-            id: 1,
-        },
-        {
-            name: "Sales & Marketing",
-            url: "#",
-            id: 2,
-        },
-        {
-            name: "Travel",
-            url: "#",
-            id: 3,
-        },
-    ],
-}
-
+  AudioWaveform,
+  Castle,
+  Command,
+  GalleryVerticalEnd,
+  History,
+  LayoutDashboard,
+  MapPin,
+  Search,
+  Settings2,
+} from 'lucide-react';
 import { SideBarProps } from '@/types';
 
-export function AppSidebar({ user, ...props }: SideBarProps){
+const dataUser = {
+  teams: [
+    {
+      name: 'Prestige Heritage',
+      logo: GalleryVerticalEnd,
+      plan: 'School',
+    },
+    {
+      name: 'Dream Maker',
+      logo: AudioWaveform,
+      plan: 'Organisator',
+    },
+    {
+      name: 'Historical Life',
+      logo: Command,
+      plan: 'Client',
+    },
+  ],
+  navMain: [
+    {
+      title: 'Carte',
+      url: '/participants',
+      icon: MapPin,
+      isActive: true,
+    },
+    {
+      title: 'Châteaux',
+      url: '/participants/chateaux',
+      icon: Castle,
+    },
+    {
+      title: 'Chasses',
+      url: '/participants/chasses',
+      icon: Search,
+    },
+    {
+      title: 'Historique',
+      url: '/participants/historique',
+      icon: History,
+    },
+    {
+      title: 'Paramètres',
+      url: '#',
+      icon: Settings2,
+      items: [
+        {
+          title: 'General',
+          url: '#',
+        },
+        {
+          title: 'Team',
+          url: '#',
+        },
+        {
+          title: 'Billing',
+          url: '#',
+        },
+        {
+          title: 'Limits',
+          url: '#',
+        },
+      ],
+    },
+  ],
+  chasses: [
+    {
+      name: 'Design Engineering',
+      url: '#',
+      id: 1,
+    },
+    {
+      name: 'Sales & Marketing',
+      url: '#',
+      id: 2,
+    },
+    {
+      name: 'Travel',
+      url: '#',
+      id: 3,
+    },
+  ],
+};
 
-    const data = user === "organisateur" ? dataOrganisateur : dataUser;
+const dataOrganisateur = {
+  teams: [
+    {
+      name: 'Prestige Heritage',
+      logo: GalleryVerticalEnd,
+      plan: 'School',
+    },
+    {
+      name: 'Dream Maker',
+      logo: AudioWaveform,
+      plan: 'Organisator',
+    },
+    {
+      name: 'Historical Life',
+      logo: Command,
+      plan: 'Client',
+    },
+  ],
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '/organisateurs/dashboard',
+      icon: LayoutDashboard,
+    },
+    {
+      title: 'Châteaux',
+      url: '/organisateurs/chateaux',
+      icon: Castle,
+    },
+    {
+      title: 'Chasses',
+      url: '#',
+      icon: Search,
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings2,
+      items: [
+        {
+          title: 'General',
+          url: '#',
+        },
+        {
+          title: 'Team',
+          url: '#',
+        },
+        {
+          title: 'Billing',
+          url: '#',
+        },
+        {
+          title: 'Limits',
+          url: '#',
+        },
+      ],
+    },
+  ],
+  chasses: [
+    {
+      name: 'Design Engineering',
+      id: 1,
+    },
+    {
+      name: 'Sales & Marketing',
+      id: 2,
+    },
+    {
+      name: 'Travel',
+      id: 3,
+    },
+  ],
+};
 
-    return (
-      <Sidebar collapsible="icon" {...props}>
-        <SidebarHeader>
-          <TeamSwitcher teams={data.teams} />
-        </SidebarHeader>
-        <SidebarContent>
-          <NavMain items={data.navMain} />
-          <NavProjects user={user} chasse={data.chasses} />
-        </SidebarContent>
-        <SidebarFooter>
-          <NavUser />
-        </SidebarFooter>
-        <SidebarRail />
-      </Sidebar>
-  )
+export function AppSidebar({ user, ...props }: SideBarProps) {
+  const data = user === 'organisateur' ? dataOrganisateur : dataUser;
+
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects user={user} chasse={data.chasses} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
-
