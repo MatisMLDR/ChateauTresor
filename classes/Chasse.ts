dfb109f53c6eddb0c3522dfa85f60ba9ed2366f6
Rahim BOUGHENDJOUR <rahim.boughendjour@etu.univ-grenoble-alpes.fr>
2025-01-14 12:09:02 +0100

    Changement des classes, des types de Chasse et Enigme, du formulaire de création de chasse et de l'API de chasse

--- classes/Chasse.ts
+++ classes/Chasse.ts
@@ -25,7 +25,7 @@ class Chasse {
   private id_equipe: UUID | null;
 
   constructor(chasse: ChasseType) {
-    this.id_chasse = chasse.id_chasse ?? -1;
+    this.id_chasse = chasse.id_chasse as UUID;
     this.titre = chasse.titre ?? "Nouvelle Chasse";
     this.capacite = chasse.capacite ?? 0;
     this.description = chasse.description ?? "Pas de description";
